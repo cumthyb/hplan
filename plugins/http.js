7a7a83d5fc08f1472fe5a3ba78df22b0869a5e4e
hybKrSpace <hyb@MacBook-Pro-HYB.local>
2018-10-10 17:11:03 +0800

    feat: 响应拦截 token过期重定向至登陆

--- plugins/http.js
+++ plugins/http.js
@@ -9,20 +9,19 @@ const env = process.env.NODE_ENV;
 
 axios.defaults.withCredentials = true;
 
-axios.defaults.mode = 'cors';
+axios.defaults.mode = "cors";
 
-
-axios.interceptors.request.use(config => {
+// http request 拦截器
+axios.interceptors.request.use(
+  config => {
     // console.log(config,"=====")
-    if (config.method == 'post' || config.method == 'put') {
-        if (!config.data.isPut) {
-            let data = Qs.stringify(config.data);
-            config.data = data;
-        } else {
-            delete config.data.isPut;
-        }
-
-
+    if (config.method == "post" || config.method == "put") {
+      if (!config.data.isPut) {
+        let data = Qs.stringify(config.data);
+        config.data = data;
+      } else {
+        delete config.data.isPut;
+      }
     }
     // if (config.url.indexOf('mockjs') !== -1) {
     //     config.baseURL = 'http://rap.krspace.cn';
@@ -32,173 +31,185 @@ axios.interceptors.request.use(config => {
     // } else {
     //     config.baseURL = '/';
     // }
-    config.baseURL = 'http://127.0.0.1:4000' //后端接口访问4000端口 由nginx配置代理
+    config.baseURL = "http://127.0.0.1:4000"; //后端接口访问4000端口 由nginx配置代理
     // config.baseURL = 'http://118.89.230.43:3011' //后端接口访问4000端口 由nginx配置代理
-    return config
-}, error => {
-    return Promise.reject(error)
-})
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
 
+// http response 拦截器
+axios.interceptors.response.use(
+  response => {
+    //这个判断是关键，如果返回登陆页面内容了，就刷新当前页面，经后端处理就会跳转到登陆页面了
+    if (response.status === 301) {
+      window.location.replace("/login");
+    }
+    return response;
+  },
+  error => {
+    return Promise.reject(error.response); // 返回接口返回的错误信息
+  }
+);
 
 function toType(obj) {
-    return ({}).toString.call(obj).match(/\s([a-zA-Z]+)/)[1].toLowerCase()
+  return {}.toString
+    .call(obj)
+    .match(/\s([a-zA-Z]+)/)[1]
+    .toLowerCase();
 }
 // 参数过滤函数
 function filterNull(o) {
-    for (var key in o) {
-        if (o[key] === null) {
-            delete o[key];
-        }
-        if (toType(o[key]) === "string") {
-            o[key] = o[key].trim();
-        } else if (toType(o[key]) === "object") {
-            o[key] = filterNull(o[key]);
-        } else if (toType(o[key]) === "array") {
-            o[key] = filterNull(o[key]);
-        }
+  for (var key in o) {
+    if (o[key] === null) {
+      delete o[key];
     }
-    return o;
+    if (toType(o[key]) === "string") {
+      o[key] = o[key].trim();
+    } else if (toType(o[key]) === "object") {
+      o[key] = filterNull(o[key]);
+    } else if (toType(o[key]) === "array") {
+      o[key] = filterNull(o[key]);
+    }
+  }
+  return o;
 }
 
 function check401(res) {
-    res = res.data;
-    if (res.code === -4011) {
-        const redirectUrl = encodeURIComponent(window.location.href);
-        if (typeof(Storage) !== "undefined") {
-            if (sessionStorage.user) {
-                sessionStorage.navs = '';
-                sessionStorage.user = '';
-            }
-
-        }
-        window.location.href = `/new/login.html?RU=${redirectUrl}`;
-    } else if (res.code === -4033) {
-        // console.log('您没有操作权限，请联系管理员')
+  res = res.data;
+  if (res.code === -4011) {
+    const redirectUrl = encodeURIComponent(window.location.href);
+    if (typeof Storage !== "undefined") {
+      if (sessionStorage.user) {
+        sessionStorage.navs = "";
+        sessionStorage.user = "";
+      }
     }
-    return res;
+    window.location.href = `/new/login.html?RU=${redirectUrl}`;
+  } else if (res.code === -4033) {
+    // console.log('您没有操作权限，请联系管理员')
+  }
+  return res;
 }
 
-
-
 export default {
-    get: (url, params, success, failure) =>
-        new Promise((resolve, reject) => {
-            if (params) {
-                params = filterNull(params);
-            }
-            if (!APIS[url].url) {
-                return;
-            }
-            axios
-                .get(APIS[url].url, { params: params })
-                .then(check401)
-                .then(function(data) {
-                    if (parseInt(data.code) > 0) {
-                        success && success(data);
-                        resolve(data);
-                    } else {
-                        failure && failure(data);
-                        reject(data);
-                    }
-                })
-                .catch(function(error) {
-                    if (error && error.response && error.response.data) {
-                        error = error.response.data;
-                        failure && failure(error);
-                        reject(error);
-                    }
-
-                });
-        }),
-    post: (url, params, success, failure) =>
-        new Promise((resolve, reject) => {
-            if (params) {
-                params = filterNull(params);
-            }
-            if (!APIS[url].url) {
-                return;
-            }
-            axios
-                .post(APIS[url].url, params)
-                .then(check401)
-                .then(function(response) {
-                    if (parseInt(response.code) > 0) {
-                        success && success(response);
-                        resolve(response);
-                    } else {
-                        if (failure) {
-                            failure && failure(response);
-                        } else {
-                            return Promise.reject(response);
-                        }
-                    }
-                })
-                .catch(function(error) {
-                    if (error && error.response && error.response.data) {
-                        error = error.response.data;
-                        failure && failure(error);
-                        reject && reject(error);
-                    } else {
-                        reject(error);
-                    }
-                });
-        }),
-    put: (url, params, success, failure) =>
-        new Promise((resolve, reject) => {
-            if (params) {
-                params = filterNull(params);
-            }
-            if (!APIS[url].url) {
-                return;
-            }
-            axios
-                .put(APIS[url].url, params)
-                .then(check401)
-                .then(function(response) {
-                    if (parseInt(response.code) > 0) {
-                        success && success(response);
-                        resolve(response);
-                    } else {
-                        failure && failure(response);
-                        reject(response);
-                    }
-                })
-                .catch(function(error) {
-                    if (error && error.response && error.response.data) {
-                        error = error.response.data;
-                        failure && failure(error);
-                        reject(error);
-                    }
-
-                });
-        }),
-    delete: (url, params, success, failure) =>
-        new Promise((resolve, reject) => {
-            if (params) {
-                params = filterNull(params);
-            }
-            if (!APIS[url].url) {
-                return;
-            }
-            axios
-                .delete(APIS[url].url, { params: params })
-                .then(check401)
-                .then(function(data) {
-                    if (parseInt(data.code) > 0) {
-                        success && success(data);
-                        resolve(data);
-                    } else {
-                        failure && failure(data);
-                        reject(data);
-                    }
-                })
-                .catch(function(error) {
-                    if (error && error.response && error.response.data) {
-                        error = error.response.data;
-                        failure && failure(error);
-                        reject(error);
-                    }
-
-                });
+  get: (url, params, success, failure) =>
+    new Promise((resolve, reject) => {
+      if (params) {
+        params = filterNull(params);
+      }
+      if (!APIS[url].url) {
+        return;
+      }
+      axios
+        .get(APIS[url].url, { params: params })
+        .then(check401)
+        .then(function(data) {
+          if (parseInt(data.code) > 0) {
+            success && success(data);
+            resolve(data);
+          } else {
+            failure && failure(data);
+            reject(data);
+          }
         })
+        .catch(function(error) {
+          if (error && error.response && error.response.data) {
+            error = error.response.data;
+            failure && failure(error);
+            reject(error);
+          }
+        });
+    }),
+  post: (url, params, success, failure) =>
+    new Promise((resolve, reject) => {
+      if (params) {
+        params = filterNull(params);
+      }
+      if (!APIS[url].url) {
+        return;
+      }
+      axios
+        .post(APIS[url].url, params)
+        .then(check401)
+        .then(function(response) {
+          if (parseInt(response.code) > 0) {
+            success && success(response);
+            resolve(response);
+          } else {
+            if (failure) {
+              failure && failure(response);
+            } else {
+              return Promise.reject(response);
+            }
+          }
+        })
+        .catch(function(error) {
+          if (error && error.response && error.response.data) {
+            error = error.response.data;
+            failure && failure(error);
+            reject && reject(error);
+          } else {
+            reject(error);
+          }
+        });
+    }),
+  put: (url, params, success, failure) =>
+    new Promise((resolve, reject) => {
+      if (params) {
+        params = filterNull(params);
+      }
+      if (!APIS[url].url) {
+        return;
+      }
+      axios
+        .put(APIS[url].url, params)
+        .then(check401)
+        .then(function(response) {
+          if (parseInt(response.code) > 0) {
+            success && success(response);
+            resolve(response);
+          } else {
+            failure && failure(response);
+            reject(response);
+          }
+        })
+        .catch(function(error) {
+          if (error && error.response && error.response.data) {
+            error = error.response.data;
+            failure && failure(error);
+            reject(error);
+          }
+        });
+    }),
+  delete: (url, params, success, failure) =>
+    new Promise((resolve, reject) => {
+      if (params) {
+        params = filterNull(params);
+      }
+      if (!APIS[url].url) {
+        return;
+      }
+      axios
+        .delete(APIS[url].url, { params: params })
+        .then(check401)
+        .then(function(data) {
+          if (parseInt(data.code) > 0) {
+            success && success(data);
+            resolve(data);
+          } else {
+            failure && failure(data);
+            reject(data);
+          }
+        })
+        .catch(function(error) {
+          if (error && error.response && error.response.data) {
+            error = error.response.data;
+            failure && failure(error);
+            reject(error);
+          }
+        });
+    })
 };
